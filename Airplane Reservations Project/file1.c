8cbc8c15f755b3ed22f4f2b662666b4de5507952
aencheng <111991942+aencheng@users.noreply.github.com>
2023-01-24 18:52:45 -0500

    Create file1.c

---
+++ Airplane Reservations Project/file1.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+
+char passengersFN[25][15];
+char passengersLN[25][15];
+void createPassengerFirstNameList(){
+  strcpy(passengersFN[0], "Andrew");
+  strcpy(passengersFN[1], "Chris");
+  strcpy(passengersFN[2], "Megan");
+  strcpy(passengersFN[3], "Drew");
+  strcpy(passengersFN[4], "Grayson");
+  strcpy(passengersFN[5], "Ashley");
+  strcpy(passengersFN[6], "Ash");
+  strcpy(passengersFN[7], "Margaret");
+  strcpy(passengersFN[8], "Maddy");
+  strcpy(passengersFN[9], "Christy");
+  strcpy(passengersFN[10], "Camden");
+  strcpy(passengersFN[11], "Isabelle");
+  strcpy(passengersFN[12], "Mabel");
+  strcpy(passengersFN[13], "Olivia");
+  strcpy(passengersFN[14], "Jenna");
+  strcpy(passengersFN[15], "Joseline");
+  strcpy(passengersFN[16], "Lily");
+  strcpy(passengersFN[17], "Kathryn");
+  strcpy(passengersFN[18], "Matt");
+  strcpy(passengersFN[19], "Mason");
+  strcpy(passengersFN[20], "Mac");
+  strcpy(passengersFN[21], "Jimothy");
+  strcpy(passengersFN[22], "Eric");
+  strcpy(passengersFN[23], "Jia");
+  strcpy(passengersFN[24], "James");
+}
+
+void createPassengerLastNameList(){
+  strcpy(passengersLN[0], "Cheng");
+  strcpy(passengersLN[1], "Leonardo");
+  strcpy(passengersLN[2], "Dixson");
+  strcpy(passengersLN[3], "Karnfel");
+  strcpy(passengersLN[4], "List");
+  strcpy(passengersLN[5], "Ortz");
+  strcpy(passengersLN[6], "Ketchum");
+  strcpy(passengersLN[7], "Speir");
+  strcpy(passengersLN[8], "Mastella");
+  strcpy(passengersLN[9], "Pham");
+  strcpy(passengersLN[10], "Port");
+  strcpy(passengersLN[11], "Marleen");
+  strcpy(passengersLN[12], "Syrup");
+  strcpy(passengersLN[13], "Pearson");
+  strcpy(passengersLN[14], "Nicole");
+  strcpy(passengersLN[15], "Hernandez");
+  strcpy(passengersLN[16], "Rodriguez");
+  strcpy(passengersLN[17], "Morning");
+  strcpy(passengersLN[18], "Foutz");
+  strcpy(passengersLN[19], "Williams");
+  strcpy(passengersLN[20], "Cooley");
+  strcpy(passengersLN[21], "Nun");
+  strcpy(passengersLN[22], "Nam");
+  strcpy(passengersLN[23], "Qi");
+  strcpy(passengersLN[24], "Harden");
+}
